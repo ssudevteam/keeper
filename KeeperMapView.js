c81fbe879c0bea5ba6f188b9feb368c2d11707fb
Joel <brolgedu@gmail.com>
2023-10-20 21:30:06 -0700

    Added ability to create new points of interest and customer Callouts when a point is selected.

--- KeeperMapView.js
+++ KeeperMapView.js
@@ -1,36 +1,22 @@
 import {useRef, useState} from "react";
-import MapView, {Marker, PROVIDER_GOOGLE} from "react-native-maps";
-import * as FileSystem from "expo-file-system";
-import {shareAsync} from "expo-sharing";
-import {View} from "react-native";
+import MapView, {Callout, Marker, PROVIDER_GOOGLE} from "react-native-maps";
+import {Button, View, Text} from "react-native";
 import {StatusBar} from "expo-status-bar";
 import {Styles} from "./styles";
 
-let locationsOfInterest = [
-    {
-        title: "First",
-        location: {
-            latitude: -27.2,
-            longitude: 145
-        },
-        description: "My First Marker"
-    },
-    {
-        title: "Second",
-        location: {
-            latitude: -30.2,
-            longitude: 150
-        },
-        description: "My Second Marker"
-    }
-]
+let originLocation = [{
+    title: "SSU Campus", location: {
+        latitude: 38.34008053681795, longitude: -122.6755222789825,
+    }, description: "Charles Darwin Building"
+},]
 
 export default function KeeperMapView() {
+    const [locationsOfInterest, setLocationsOfInterest] = useState(originLocation)
     const [count, setCount] = useState(0);
-    const [draggableMarkerCoord, setDraggableMarkerCoord] = useState({
-        latitude: 38.34008053681795,
-        longitude: -122.6755222789825
-    });
+    // const [draggableMarkerCoord, setDraggableMarkerCoord] = useState({
+    //     latitude: 38.34008053681795,
+    //     longitude: -122.6755222789825
+    // });
     const mapRef = useRef();
 
     const onRegionChange = (region) => {
@@ -39,60 +25,95 @@ export default function KeeperMapView() {
 
     const showLocationsOfInterest = () => {
         return locationsOfInterest.map((item, index) => {
-            return (
-                <Marker
-                    key={index}
-                    coordinate={item.location}
-                    title={item.title}
-                    description={item.description}
-                />
-            )
+            console.log(item.title);
+            return (<Marker
+                key={index}
+                coordinate={item.location}
+                title={item.title}
+                description={item.description}
+            >
+                <Callout>
+                    <Text>{item.title}</Text>
+                    <Text>{item.description}</Text>
+                    <Text>{`Lat: ${item.location.latitude}\nLong: ${item.location.longitude})`}</Text>
+                    <Button title='Edit' onPress={() => null }/>
+                </Callout>
+            </Marker>)
         });
     };
 
-    const takeSnapshotAndShare = async () => {
-        const snapshot = await mapRef.current.takeSnapshot({width: 300, height: 300, result: 'base64'});
-        const uri = FileSystem.documentDirectory + "snapshot.png";
-        await FileSystem.writeAsStringAsync(uri, snapshot, {encoding: FileSystem.EncodingType.Base64});
-        await shareAsync(uri);
+    const getLocationOfInterest = (coordinates) => {
+        if (!coordinates || !coordinates.longitude || !coordinates.latitude) {
+            console.error('Coordinates must have a longitude and latitude');
+        }
+        const latitude = coordinates.latitude;
+        const longitude = coordinates.longitude;
+        for (const point of locationsOfInterest) {
+            if (point.location.latitude === latitude && point.location.longitude === longitude) {
+                return point;
+            }
+        }
+        return null;
+    }
+
+    const handleOnPress = (state) => {
+        const event = state['nativeEvent'];
+        const location = getLocationOfInterest(event.coordinate);
+        if (location) {
+            // Show annotation and/or edit menu, etc.
+            console.log(location);
+        } else {
+            addLocationOfInterest(event.coordinate);
+        }
     };
 
-    return (
-        <View style={Styles.container}>
-            <MapView
-                provider={PROVIDER_GOOGLE}
-                ref={mapRef}
-                style={Styles.map}
-                onRegionChange={onRegionChange}
-                initialRegion={{
-                    latitude: 38.34008053681795,
-                    longitude: -122.6755222789825,
-                    latitudeDelta: 27.499085419977938,
-                    longitudeDelta: 15.952148000000022,
-                }}
-                // customMapStyle={mapJson}
-            >
-                {showLocationsOfInterest()}
-                <Marker
-                    draggable
-                    pinColor='#0000ff'
-                    coordinate={draggableMarkerCoord}
-                    onDragEnd={(e) => setDraggableMarkerCoord(e.nativeEvent.coordinate)}
-                />
-                {/*<Marker*/}
-                {/*    pinColor='#00ff00'*/}
-                {/*    coordinate={{latitude: -35, longitude: 147}}*/}
-                {/*>*/}
-                {/*    <Callout>*/}
-                {/*        <Text>Count: {count}</Text>*/}
-                {/*        <Button title='Increment Count' onPress={() => setCount(count + 1)}/>*/}
-                {/*        <Button title='Take Snapshot and Share' onPress={takeSnapshotAndShare}/>*/}
-                {/*    </Callout>*/}
-                {/*</Marker>*/}
-                {/*<Text style={styles.mapOverlay}>Longitude: {draggableMarkerCoord.longitude},*/}
-                {/*    latitude: {draggableMarkerCoord.latitude}</Text>*/}
-            </MapView>
-            <StatusBar style="auto"/>
-        </View>
-    );
+    const addLocationOfInterest = (coordinates) => {
+        if (!coordinates || !coordinates.longitude || !coordinates.latitude) {
+            console.error('Coordinates must have a longitude and latitude');
+        }
+        console.log(coordinates);
+        const latitude = coordinates.latitude;
+        const longitude = coordinates.longitude;
+        const newLocation = {
+            title: `Hive #${count + 1}`, location: {
+                latitude: latitude, longitude: longitude
+            }, description: ""
+        };
+        setLocationsOfInterest(prevLocations => [...prevLocations, newLocation]);
+        setCount(count + 1);
+    }
+
+    return (<View style={Styles.container}>
+        <MapView
+            provider={PROVIDER_GOOGLE}
+            ref={mapRef}
+            style={Styles.map}
+            onRegionChange={onRegionChange}
+            initialRegion={{
+                latitude: 38.34008053681795, longitude: -122.6755222789825, latitudeDelta: 0.5, longitudeDelta: 0.3,
+            }}
+            onPress={handleOnPress}
+            // customMapStyle={mapJson}
+        >
+            {showLocationsOfInterest()}
+            {/*<Marker*/}
+            {/*    draggable*/}
+            {/*    pinColor='#0000ff'*/}
+            {/*    coordinate={draggableMarkerCoord}*/}
+            {/*    onDragEnd={(e) => setDraggableMarkerCoord(e.nativeEvent.coordinate)}*/}
+            {/*/>*/}
+            {/*<Marker*/}
+            {/*    pinColor='#00ff00'*/}
+            {/*    coordinate={{latitude: -35, longitude: 147}}*/}
+            {/*>*/}
+            <Callout>
+                <Text>Count: {count}</Text>
+                <Button title='Increment Count' onPress={() => setCount(count + 1)}/>
+            </Callout>
+            {/*</Marker>*/}
+            {/*<Text style={styles.mapOverlay}>Longitude: {draggableMarkerCoord.longitude},*/}
+            {/*    latitude: {draggableMarkerCoord.latitude}</Text>*/}
+        </MapView>
+        <StatusBar style="auto"/>
+    </View>);
 }
